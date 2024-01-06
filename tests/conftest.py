82d96e72cc15fc05911559d372cde650cb3cbe34
Max Trukhinov <maks.truxinov@yandex.ru>
2024-01-07 00:46:38 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,64 @@
 import sys
 from pathlib import Path
 
+import pytestfrom yacut.models import URLMap
+from yacut import app, db
+import sys
+from pathlib import Path
+
 import pytest
+from dotenv import load_dotenv
+from mixer.backend.flask import mixer as _mixer
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(BASE_DIR))
+
+
+@pytest.fixture
+def default_app():
+    with app.app_context():
+        yield app
+
+
+@pytest.fixture
+def _app(tmp_path):
+    db_path = tmp_path / 'test_db.sqlite3'
+    db_uri = 'sqlite:///' + str(db_path)
+    app.config.update({
+        'TESTING': True,
+        'SQLALCHEMY_DATABASE_URI': db_uri,
+        'WTF_CSRF_ENABLED': False,
+    })
+    with app.app_context():
+        db.create_all()
+    yield app
+    db.drop_all()
+    db.session.close()
+    db_path.unlink()
+
+
+@pytest.fixture
+def client(_app):
+    return _app.test_client()
+
+
+@pytest.fixture
+def cli_runner():
+    return app.test_cli_runner()
+
+
+@pytest.fixture
+def mixer():
+    _mixer.init_app(app)
+    return _mixer
+
+
+@pytest.fixture
+def short_python_url(mixer):
+    return mixer.blend(URLMap, original='https://www.python.org', short='py')
+
 from dotenv import load_dotenv
 from mixer.backend.flask import mixer as _mixer
 
