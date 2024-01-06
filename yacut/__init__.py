4c2e673a2f7f8c4590ebb6e2bec3539445ecf0d7
Максим Трухинов <maks.truxinov@yandex.ru>
2024-01-07 00:17:14 +0300

    Minor changes

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,24 +1,13 @@
-import os
-from flask import Flask, render_template
+from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-# from settings import Config
+from settings import Config
 
 app = Flask(__name__)
-# app.config.from_object(Config)
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
-
-
-from . import api_views, forms, models, views
+from . import api_views, error_handlers, forms, models, views
